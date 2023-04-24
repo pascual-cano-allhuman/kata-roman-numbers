68b06aae0246ecc945b2d13af832337473bd7014
Pascual Cano <pascual@allhuman.com>
2023-04-24 10:40:10 +0200

    Includes conversion for 5, 6, 7 and 8

--- src/roman.ts
+++ src/roman.ts
@@ -1,3 +1,4 @@
 export const numberToRoman = (n: number): string => {
+	if (n >= 5) return ["V", numberToRoman(n - 5)].join("");
 	return "".padStart(n, "I");
 };
--- tests/roman.spec.ts
+++ tests/roman.spec.ts
@@ -10,4 +10,16 @@ describe("Roman numbers tests", () => {
 	test(`return III for 3`, () => {
 		expect(numberToRoman(3)).toBe("III");
 	});
+	test(`return V for 5`, () => {
+		expect(numberToRoman(5)).toBe("V");
+	});
+	test(`return VI for 6`, () => {
+		expect(numberToRoman(6)).toBe("VI");
+	});
+	test(`return VII for 7`, () => {
+		expect(numberToRoman(7)).toBe("VII");
+	});
+	test(`return VIII for 8`, () => {
+		expect(numberToRoman(8)).toBe("VIII");
+	});
 });
